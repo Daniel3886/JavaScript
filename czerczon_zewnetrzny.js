63b39ddae20e30d9f9556835f16de38f9c4ac093
Daniel3886 <112687208+Daniel3886@users.noreply.github.com>
2023-09-07 08:10:11 +0100

    Update czerczon_zewnetrzny.js

--- czerczon_zewnetrzny.js
+++ czerczon_zewnetrzny.js
@@ -1,2 +1,2 @@
-document.write("ostatnia modyfikacja strony".fontcolor("green").object.style.fontsize(6)+"<br>");
+document.write("ostatnia modyfikacja strony".fontcolor("green").object.style.textDecoration.fontsize(6)+"<br>");
 document.write(document.lastModified);
